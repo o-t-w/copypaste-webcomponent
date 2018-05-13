d46d942c9c3dd498d0ab081812b958b58ab4124f
Oliver Williams <oliver.williams@springernature.com>
2018-05-13 18:32:54 +0100

    remove spellchecking red underline

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ let copyPasteTemplate = document.createElement('template');
   </style>
 	
 	<div class="copypaste">
-            <span contenteditable="true"></span>
+            <span spellcheck="false" contenteditable="true"></span>
             <button>Copy <?xml version="1.0" encoding="UTF-8" standalone="no"?>
               <svg class="opacity0" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                   <title>iconography/status-complete</title>
